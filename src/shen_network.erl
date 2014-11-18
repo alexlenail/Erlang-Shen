6825a350bd293cc48fd694189282052c03f33f16
Sunjay Bhatia <sunjaybhatia@hotmail.com>
2014-11-18 01:22:10 -0500

    more network thoughts

--- src/shen_network.erl
+++ src/shen_network.erl
@@ -1,13 +1,22 @@
 -module (shen_network).
--export ([start/2]).
+-export ([start/3]).
 
 
+start(TrainSet, TestSet, HiddenLayers) ->
 
-% NetworkArchitecture is a list of Hidden Layer Dimensions. 
-start(DataInfo, NetworkArchitecture) -> 
+	case shen_parse:arff(TrainSet) of
+		{error, _Reason} -> {error, "Invalid training data file"};
+		{ok, NumAttrs, Classes, Instances} ->
+			InputLayer = spawn_neurons(NumAttrs, Classes),
+			% gradient descent loop, train network
+			% get training data
+			% test run and calculate accuracy
+	end,
 
-	{TrainSet, TestSet} = shen_parse:get_data(DataInfo),
 
+% spawn neurons and return pid list of input layer
+% maybe be need number of training instances?
+spawn_neurons(NumAttrs, Classes) ->
 	Network = [[spawn(neuron, start, []) || X <- lists:seq(Dimension)] || Dimension <- NetworkArchitecture],
 
 	lists:foldl(fun(Layer, LayerAfter) -> lists:map(fun(Neuron) -> Neuron ! LayerAfter, Layer end, [], Network),
@@ -24,11 +33,4 @@ start(DataInfo, NetworkArchitecture) ->
 	% Send the last hidden layer these Pids, and send these Pids the Layerbefore. 
 
 
-
-% learn
-    % send data to input layer
-
-
 % bias units?
-
-
