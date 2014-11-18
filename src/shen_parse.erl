9843995cf9b2473aebb48c17965e58df126ed1a5
Sunjay Bhatia <sunjaybhatia@hotmail.com>
2014-11-18 01:07:53 -0500

    naive arff parser, still need to cast instances to numeric

--- src/shen_parse.erl
+++ src/shen_parse.erl
@@ -1,9 +1,51 @@
 -module(shen_parse).
--export([get_sdfsd/1]).
+-export([arff/1]).
 
 
-% we can handle 2 types of data sets, arff and NIST standardized ocr data
+arff(Fname) ->
+    case file:open(Fname, [read, raw]) of
+        {error, Reason} -> {error, Reason};
+        {ok, Fd} -> arff_line_by_line(Fd, 0, [], [])
+    end.
 
-get_data({arff, TrainSet, TestSet}) -> ok;
-get_data({ocr_nist, TrainSet, TestSet}) -> ok;
-get_data(_) -> {error, invalid_data}.
+
+arff_line_by_line(Fd, NumAttrs, Classes, Instances) ->
+    case file:read_line(Fd) of
+        {error, Reason} -> {error, Reason};
+        eof ->
+            file:close(Fd),
+            {ok, NumAttrs, Classes, Instances};
+        {ok, Data} ->
+            L = string:to_lower(string:strip(Data)),
+            Line = string:substr(L, 1, string:len(L)-1),
+            case arff_line_type(Line) of
+                skipline -> arff_line_by_line(Fd, NumAttrs, Classes, Instances);
+                {classes, UnsplitClasses} -> arff_line_by_line(Fd, NumAttrs, arff_get_classes(UnsplitClasses), Instances);
+                attribute -> arff_line_by_line(Fd, NumAttrs+1, Classes, Instances);
+                instance -> arff_line_by_line(Fd, NumAttrs, Classes, [arff_get_instance(Line) | Instances])
+            end
+    end.
+
+
+arff_line_type("\n") -> skipline;
+arff_line_type("@data") -> skipline;
+arff_line_type(Line) ->
+    case string:chr(Line, $%) of
+        1 -> skipline;
+        _ ->
+            case string:str(Line, "@attribute") of
+                1 ->
+                    SplitLine = string:tokens(Line, " "),
+                    case lists:nth(2, SplitLine) of
+                        "class" -> {classes, lists:nth(3,SplitLine)};
+                        _ -> attribute
+                    end;
+                _ -> instance
+            end
+    end.
+
+
+arff_get_classes(UnsplitClasses) -> string:tokens(string:substr(UnsplitClasses, 2, string:len(UnsplitClasses)-2), ",").
+
+
+arff_get_instance(Line) -> string:tokens(Line, ",").
